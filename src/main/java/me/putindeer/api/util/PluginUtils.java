dd93ad4359cb03821058b2032c0a49a9e03a7e72
Diego <putindeergaming@gmail.com>
2025-11-07 00:48:22 -0300

    soy tonto

--- src/main/java/me/putindeer/api/util/PluginUtils.java
+++ src/main/java/me/putindeer/api/util/PluginUtils.java
@@ -723,7 +723,7 @@ public class PluginUtils {
     }
     //endregion
 
-    private boolean isTool(ItemStack item) {
+    public boolean isTool(ItemStack item) {
         String name = item.getType().name();
         return name.endsWith("_AXE") || name.endsWith("_PICKAXE") || name.endsWith("_HOE") || name.endsWith("_SHOVEL")
                 || item.getType() == Material.SHEARS || item.getType() == Material.FISHING_ROD || item.getType() == Material.FLINT_AND_STEEL;
